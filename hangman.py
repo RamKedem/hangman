47dba402a80dc48a501b741157d7bebb39fdb770
Ram Kedem <ram.kdm@gmail.com>
2020-04-18 15:21:42 +0300

    Modified Functions

--- hangman.py
+++ hangman.py
@@ -5,8 +5,8 @@ DEBUG = False
 HANG_GRAPHICS = list(hang_graphics())
 ALLOWED_GUESSES = len(HANG_GRAPHICS)
 PLACEHOLDER = '-'
-VALID_YES_REPLIES = ['YES','Y']
-VALID_NO_REPLIES = ['NO','N']
+VALID_YES_REPLIES = ['YES', 'Y']
+VALID_NO_REPLIES = ['NO', 'N']
 
 
 class HangMan(object):
@@ -14,130 +14,96 @@ class HangMan(object):
         self.chosen_word = generate_word() if word is None else word
         self.number_of_guesses = len(HANG_GRAPHICS)
         self.masked_word = PLACEHOLDER * len(self.chosen_word)
-        self.g_string = ''
-        self.used_guesses = set()
+        self.used_guesses = set()  # good use of sets, most programmers don't think to use them! :)
         self.wrong_guesses = set()
         self.another_round = True
 
     @staticmethod
     def begin_user_interaction():
         clear()
-        print(HANG_GRAPHICS[len(HANG_GRAPHICS)-1] + '\n')
+        print(HANG_GRAPHICS[len(HANG_GRAPHICS) - 1] + '\n')
         print('Welcome to Hangman !\n')
-        print('During this game you\'ll be trying to guess a word.')
-        print('You should try and guess it by suggesting letters within {} turns'\
-              .format(len(HANG_GRAPHICS)))
-        print('In each turn you\'re welcome to guess one letter or the entire word\n')
-
-    @staticmethod
-    def check_valid_y_n_answer(user_reply):
-        user_reply = user_reply.upper()
-        if user_reply in VALID_NO_REPLIES:
-            return False
-        elif user_reply in VALID_YES_REPLIES:
-            return True
-
-    @staticmethod
-    def ask_begin_game():
-        user_input = input('Ready to begin ? : ')
-        return user_input
-
-    def check_guess_already_used(self, user_input):
-        if user_input in self.used_guesses:
-            return True
-        else:
-            return False
-
-    def check_wrong_guess_already_tried(self, user_input):
-        if user_input in self.wrong_guesses:
-            return True
-        else:
-            return False
-
-    @staticmethod
-    def check_only_one_letter(user_input):
-        if len(user_input) == 1:
-            return True
-        else:
-            return False
+        print("During this game you'll be trying to guess a word.")
+        print(
+            f"You should try and guess it by suggesting letters within {len(HANG_GRAPHICS)} turns")  # f-strings allow you to add variables in-line easily.
+        print("In each turn you're welcome to guess one letter or the entire word\n")
 
     def display_progress(self):
         clear()
         print(HANG_GRAPHICS[len(HANG_GRAPHICS) - self.number_of_guesses] + '\n')
-        print("[{} guesses left]\nThe word now looks like this : {}"
-              .format(self.number_of_guesses, self.masked_word))
+        print(f"[{self.number_of_guesses} guesses left]\nThe word now looks like this : {self.masked_word}")
 
     def accept_and_examine_user_input(self):
         guess = input("Please type your guess : ")
         while True:
-            if self.check_guess_already_used(guess):
+            if guess in self.used_guesses:
                 print('You already used this guess')
-            elif self.check_wrong_guess_already_tried(guess):
+            elif guess in self.wrong_guesses:
                 print('You already tried this guess')
             else:
                 return guess
             guess = input("Take another guess : ")
 
-    def check_guess(self):
-        if self.check_only_one_letter(self.g_string):
-            if self.g_string in self.chosen_word:
-                return True
-            else:
-                return False
-        else:
-            if self.g_string == self.chosen_word:
-                return True
-            else:
-                return False
+    def check_guess(self, guess):
+        if len(guess) == 1:
+            return guess in self.chosen_word
+        return guess == self.chosen_word
 
-    def correct_guess_operations(self):
-        if self.check_only_one_letter(self.g_string):
+    def correct_guess_operations(self, guess):
+        if len(guess) == 1:
             loc = 0
             masked_list = list(self.masked_word)
-            for i in range(self.chosen_word.count(self.g_string)):
-                masked_list[self.chosen_word.index(self.g_string, loc)] = self.g_string
-                loc = self.chosen_word.index(self.g_string, loc) + 1
+            for i in range(self.chosen_word.count(guess)):
+                masked_list[self.chosen_word.index(guess, loc)] = guess
+                loc = self.chosen_word.index(guess, loc) + 1
             self.masked_word = ''.join(masked_list)
-            self.used_guesses.add(self.g_string)
+            self.used_guesses.add(guess)
         else:
-            self.masked_word = self.g_string
+            self.masked_word = guess
 
-    def wrong_guess_operations(self):
+    def wrong_guess_operations(self, guess):
         self.number_of_guesses -= 1
-        self.wrong_guesses.add(self.g_string)
+        self.wrong_guesses.add(guess)
 
-    def init_game(self):
-        user_initial_reply = self.ask_begin_game()
+    @staticmethod
+    def init_game():
         while True:
-            if self.check_valid_y_n_answer(user_initial_reply) is None:
-                message = 'Please type a valid input. [{}] for No, [{}] for Yes : '
-                user_initial_reply = input(message.format(', '.join(VALID_NO_REPLIES),
-                                                          ', '.join(VALID_YES_REPLIES)))
-            elif self.check_valid_y_n_answer(user_initial_reply):
-                return True
-            else:
+            return HangMan.query_for_yes_no('Ready to begin ? : ')
+
+    @staticmethod
+    def query_for_yes_no(question):
+        user_input = input(question)
+        while True:
+            user_input = user_input.upper()
+            if user_input in VALID_NO_REPLIES:
                 return False
+            if user_input in VALID_YES_REPLIES:
+                return True
+
+            message = f"Please type a valid input. [{', '.join(VALID_NO_REPLIES)}] for No, [{', '.join(VALID_YES_REPLIES)}] for Yes : "
+            user_input = input(message)
 
     def end_round(self):
         if self.masked_word == self.chosen_word:
             print('Good Job! You guessed the word')
         else:
-            print('You\'ve failed to guess the word : {}'
-                  .format(self.chosen_word.upper()))
+            print(f"You've failed to guess the word : {self.chosen_word.upper()}")  # converted to an f-string
 
 
 def game_controller():
     game = HangMan('hello')
     game.begin_user_interaction()
-    if game.init_game():
-        while game.number_of_guesses > 0 and game.masked_word != game.chosen_word:
-            game.display_progress()
-            game.g_string = game.accept_and_examine_user_input()
-            if game.check_guess():
-                game.correct_guess_operations()
-            else:
-                game.wrong_guess_operations()
-        game.end_round()
+    if not HangMan.init_game():
+        return
+
+    while game.number_of_guesses > 0 and game.masked_word != game.chosen_word:
+        game.display_progress()
+        guess = game.accept_and_examine_user_input()
+        if game.check_guess(guess):
+            game.correct_guess_operations(guess)
+        else:
+            game.wrong_guess_operations(guess)
+    game.end_round()
 
 
 if __name__ == "__main__":
--- tests/test_hangman.py
+++ tests/test_hangman.py
@@ -1,9 +1,11 @@
 import hangman
 import unittest
+from unittest.mock import patch
+
 import random
 import string
 
-test = hangman.HangMan('hello')
+test = hangman.HangMan('foo')
 
 
 def random_string(lower_bound=0, upper_bound=5):
@@ -17,65 +19,27 @@ class TestHangman(unittest.TestCase):
     def test_check_valid_y_n_answer(self):
         correct_y_replies = {'y', 'Y', 'YES', 'yes', 'Yes', 'yEs'}
         correct_n_replies = {'N', 'n', 'NO', 'no', 'No', 'nO'}
-        all_possible_replies = correct_y_replies.union(correct_n_replies)
-        wrong_replies = {random_string() for i in range(15)}
-        wrong_replies.difference_update(all_possible_replies)
 
         for reply in correct_y_replies:
-            self.assertTrue(test.check_valid_y_n_answer(reply))
+            with (patch('builtins.input', return_value=reply)):
+                self.assertTrue(test.query_for_yes_no('foo'))
         for reply in correct_n_replies:
-            self.assertFalse(test.check_valid_y_n_answer(reply))
-        for reply in wrong_replies:
-            self.assertIsNone(test.check_valid_y_n_answer(reply))
+            with (patch('builtins.input', return_value=reply)):
+                self.assertFalse(test.query_for_yes_no('foo'))
 
-    def test_check_guess_already_used(self):
-        guesses = {random_string() for i in range(10)}
-        test.used_guesses = guesses
-        for guess in guesses:
-            self.assertTrue(test.check_guess_already_used(guess))
+    def test_initially_8_failures_are_allowed(self):
+        self.assertEqual(test.number_of_guesses, 8)
 
-    def test_check_wrong_guess_already_tried(self):
-        guesses = {random_string() for i in range(10)}
-        test.wrong_guesses  = guesses
-        for guess in guesses:
-            self.assertTrue(test.check_wrong_guess_already_tried(guess))
+    def test_initially_no_letters_are_guessed(self):
+        words = [random_string(0,10) for _ in range(10)]
+        for word in words:
+            game_test = hangman.HangMan(word)
+            self.assertEqual(game_test.masked_word,hangman.PLACEHOLDER * len(game_test.chosen_word))
 
-    def test_check_only_one_letter(self):
-        guesses = [random_string(1,1) for i in range(10)]
-        for guess in guesses:
-            self.assertTrue(test.check_only_one_letter(guess))
-
-    def test_check_guess_one_correct_letter(self):
-        list_of_words = [random_string(2,10) for i in range(10)]
-        for word in list_of_words:
-            test.chosen_word = word
-            test.g_string = test.chosen_word[random.randint(0,len(word)-1)]
-            self.assertTrue(test.check_guess())
-
-    def test_check_guess_one_incorrect_letter(self):
-        list_of_words = [random_string(2,10) for i in range(10)]
-        for word in list_of_words:
-            test.chosen_word = word
-            while True:
-                test.g_string = random_string(1,1)
-                if test.g_string not in test.chosen_word:
-                    break
-            self.assertFalse(test.check_guess())
-
-    def test_check_guess_correct_word(self):
-        list_of_words = [random_string(2, 10) for i in range(10)]
-        for word in list_of_words:
-            test.g_string = word
-            test.chosen_word = word
-            self.assertTrue(test.check_guess())
-
-    def test_check_guess_incorrect_word(self):
-        list_of_words = [random_string(2, 10) for i in range(10)]
-        list_of_wrong_guesses = [word[::-1] for word in list_of_words]
-        for ix in range(len(list_of_words)):
-            test.g_string = list_of_words[ix]
-            test.chosen_word = list_of_wrong_guesses[ix]
-            self.assertFalse(test.check_guess())
+    def test_feeding_a_correct_letter_removes_underscores(self):
+        test_game = hangman.HangMan('hello')
+        test_game.correct_guess_operations('l')
+        self.assertEqual(test_game.masked_word, '--ll-')
 
 if __name__ == '__main__':
     unittest.main()
